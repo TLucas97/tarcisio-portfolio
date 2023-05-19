81baec5cda95b9c41476e5529c9e2d5539affea7
tarcisio <tarcisio.almeida197@gmail.com>
2023-05-19 19:13:18 -0300

    pdf path fix attempt

--- src/lib/Navigator.svelte
+++ src/lib/Navigator.svelte
@@ -123,8 +123,8 @@
 				<a
 					href={`${
 						$texts.currentLanguage === 'en'
-							? '../src/assets/cvs/TARCISIO-ALMEIDA-CV-EN.pdf'
-							: '../src/assets/cvs/TARCISIO-ALMEIDA-CV-PTBR.pdf'
+							? '../../src/assets/TARCISIO-ALMEIDA-CV-EN.pdf'
+							: '../../src/assets/TARCISIO-ALMEIDA-CV-PTBR.pdf'
 					}`}
 					type="application/pdf"
 					target="_blank"
